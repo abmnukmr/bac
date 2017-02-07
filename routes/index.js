322b44f5783d362d72553ed63526274eee3bc115
abmnukmr <abmnukmr@gmail.com>
2017-02-07 15:00:35 +0530

    new

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
+var mongoose = require('mongoose');
 var router = express.Router();
 
 /* GET home page. */
+
+mongoose.connect('mongodb://abmnukmr:12345@ds035703.mlab.com:35703/vioti', function(err){
+    if(err){
+        console.log('database not connected');
+    }
+});
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'abmnu' });
+
 });
 
 module.exports = router;
