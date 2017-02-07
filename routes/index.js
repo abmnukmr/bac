8a48ca80025305f0429047e2156f21aef3d6f761
abmnukmr <abmnukmr@gmail.com>
2017-02-07 18:45:48 +0530

    sddkfgjf

--- routes/index.js
+++ routes/index.js
@@ -5,14 +5,14 @@ var router = express.Router();
 var URL='mongodb://abmnukmr:12345@ds035703.mlab.com:35703/vioti';
 
 /* GET home page. */
-
+/*
 mongoose.connect(URL, function(err){
     if(err){
         console.log('database not connected');
     }
 
 });
-
+*/
 
 
 router.get('/', function(req, res, next) {
