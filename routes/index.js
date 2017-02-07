ebbc6de9577681cf135d1e6d235e4161909383d9
abmnukmr <abmnukmr@gmail.com>
2017-02-07 15:25:54 +0530

    new

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 
 /* GET home page. */
 
-mongoose.connect('mongodb://abmnukmr:12345@ds035703.mlab.com:35703/vioti', function(err){
+var db=mongoose.connect('mongodb://abmnukmr:12345@ds035703.mlab.com:35703/vioti', function(err){
     if(err){
         console.log('database not connected');
     }
