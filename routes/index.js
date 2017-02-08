e692d9ca54475e75834f3cdabad555db339a5f12
abmnukmr <abmnukmr@gmail.com>
2017-02-08 15:33:07 +0530

    uploaded

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,8 @@ var mongoose=require('mongoose');
 var URL='mongodb://abmnukmr:12345@ds035703.mlab.com:35703/vioti';
 const db = require('monk')(URL)
 const users = db.get('profile')
+const search = db.get('search')
+
 
 /*
 users.find(function (err,data) {
@@ -16,10 +18,24 @@ users.find(function (err,data) {
 router.get('/profile/:id', function(req, res, next) {
     users.find({'email':req.params.id },function (err,docs) {
         if(err) console.log(err);
-        else res.send(docs);
+        else res.json(docs[0]);
+    })
+});
+
+router.get('/:id', function(req, res, next) {
+    users.insert({'email':req.params.id },function (err) {
+        if(err) console.log(err);
+        else  console.log("inserted"); res.send("inserted");
 
     })
+});
+router.get('/all', function(req, res, next) {
+    users.find(function (err) {
+        if(err) console.log(err);
+        else  console.log("inserted"); res.send("inserted");
 
-  });
+    })
+});
+
 
 module.exports = router;
