ff1d3addeefb358622a01cab1e5a4d9498a4a59c
foo <foo>
2017-02-12 13:38:41 +0530

    afjhgf

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,14 @@
 var express = require('express');
+var aws = require('aws-sdk');
 var router = express.Router();
+var multerS3 = require('multer-s3');
 var multer = require('multer');
 var URL='mongodb://abmnukmr:12345@ds035703.mlab.com:35703/vioti';
 const db = require('monk')(URL)
 const users = db.get('profile')
 const search = db.get('search')
 
-
+/*
 var storage = multer.diskStorage({
     destination: function (req, file, cb) {
         cb(null, './public/images')
@@ -15,7 +17,31 @@ var storage = multer.diskStorage({
         cb(null,   Date.now()+file.originalname);
     }
 });
-var upload = multer({ storage: storage });
+*/
+//var upload = multer({ storage: storage });
+
+
+var s3 = new aws.S3( {
+    AWS_ACCESS_KEY_ID: 'AKIAIVV63WHHD6L4E4RQ',
+    AWS_SECRET_ACCESS_KEY: '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'
+ })
+
+var upload = multer({
+    storage: multerS3({
+        s3: s3,
+        bucket: 'vioti',
+        metadata: function (req, file, cb) {
+            cb(null, {fieldName: file.fieldname});
+        },
+        key: function (req, file, cb) {
+            cb(null, Date.now().toString())
+        }
+    })
+})
+
+
+
+
 
 
 router.get('/profile/upload',function (req,res,next) {
