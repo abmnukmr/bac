983d460f92bae9853d0b05634b32b98002da02a7
Manyu <manyu@Apples-MacBook-Pro.local>
2017-08-26 21:02:28 +0530

    sjhgdjhsgd

--- socket/socket.js
+++ socket/socket.js
@@ -21,6 +21,19 @@ function socket(io) {
             socket.join(room);
 
         })
+        socket.on('message', function (msg) {
+            /// var data= {
+            //   user:msg.user,
+            ///  user_naem:set_msg.username
+            // }
+
+            //  io.sockets.in(data.email).emit('message', {msg: data.message});
+            //console.log(data.email);
+            io.to(msg.email).emit('message', msg)
+
+            console.log(msg);
+
+        })
 
         socket.on('typing', function (msg) {
             /// var data= {
@@ -43,14 +56,6 @@ function socket(io) {
 
 
     });
-
-
-    socket.on('message', function (msg) {
-        io.to(msg.email).emit('message', msg)
-
-        console.log(msg);
-
-    })
 
 
 
