a8fe53bab3aea4a6c2fecd763f7eaef1d7e02f7a
abmnukmr <developer.abmnu@gmail.com>
2017-09-02 12:59:28 +0530

    aegjhgfsf

--- socket/socket.js
+++ socket/socket.js
@@ -43,7 +43,7 @@ function socket(io) {
 
             //  io.sockets.in(data.email).emit('message', {msg: data.message});
             //console.log(data.email);
-            socket.broadcast.to(msg.email).emit('typing', msg)
+            socket.broadcast.to(msg.email).emit('typingrec', msg)
 
             console.log(msg);
 
