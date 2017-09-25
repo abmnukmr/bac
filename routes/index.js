418aa4ef5b6188c160a1edeabe14a3a72ab40ba1
abmnukmr <developer.abmnu@gmail.com>
2017-09-26 01:20:58 +0530

    aws checking

--- routes/index.js
+++ routes/index.js
@@ -29,7 +29,7 @@ var s0 = new aws.S3({})
 var upload = multer({
     storage: multerS3({
         s3: s0,
-        bucket: 'vioti',
+        bucket: 'med7',
         acl: 'public-read',
 //       accessKeyId:'AKIAI3Q6TFHIZE67TWSQ',
   //     secretAccessKey: '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',
