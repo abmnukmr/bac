fab45b1b89c9a0c0d5d8c666185cd24ee9a5ecee
Abhimanyu  Kumar <abhimanyukumar@Abhimanyus-MacBook-Pro.local>
2017-11-03 03:02:01 +0530

    asmfgjhasfg

--- socket/socket.js
+++ socket/socket.js
@@ -51,7 +51,7 @@ function socket(io) {
              else {
                 var token= users.find({"email":msg.email})
                  io.to(msg.sender_mail).emit('gettomessage', msg)
-                // this.triggernotification(msg.email,msg);
+                 triggernotification(msg.email,msg);
                  console.log("Send Notification")
              }
 
@@ -113,13 +113,20 @@ function socket(io) {
 
 
 function triggernotification(email,msg){
+     var tokenn;
+    cred.find({"email":email},function (err, docs) {
+        if (err) console.log(err);
+        else {
+            res.json(docs[0]);
+             tokenn=res.json(docs[0]).token
 
-    var token= cred.find({"email":email})
+        }
+    })
 
     const FCM = require('fcm-node');
 // Replace these with your own values.
     const apiKey = "AAAAgPqR_xY:APA91bHetgjKrznUqzsIde8Arpu3nvMrmsG8h5EX_G450TjEkJxOZDsxbhNrkgzHYshtp9_xYyaTWEI7H8y0pYPwvg2EwNZfxqaFm7Xc9ixfvQS6ZoR-B5y7mo8Wws4vrCCrDuYN1N50";
-    const deviceID = token.token;
+    const deviceID = tokenn
     const fcm = new FCM(apiKey);
 
     const message = {
